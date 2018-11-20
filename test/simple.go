f4afc796fee163779a012f958b8a04ce7e90405a
Sebastião Pereira <ex13745@alunos.uevora.pt>
2018-11-20 19:02:55 +0000

    simple.go setCookie

--- test/simple.go
+++ test/simple.go
@@ -3,31 +3,28 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"io"
 	"log"
-
-	//"log"
 	"net/http"
+	"os" // For file storing
 	"strings"
-	//"strconv"
-	// "database/sql" // Sql import
-	//"io"  // For file usage
+	"time" // For cookie but could also serve timestamp on pages
+	// "database/sql"
 )
 
 /*
 Change prints to log
 */
 
-
-/*func setMyCookie(w http.ResponseWriter) {
-	cookie := http.Cookie(Name:"testCookie", Value:"testValue")
+func setMyCookie(w http.ResponseWriter) {
+	expiration := time.Now().Add(365 * 24 * time.Hour)
+	cookie := http.Cookie{Name: "testCookie", Value: "testValue", Expires: expiration}
 	http.SetCookie(w, &cookie)
-}*/
-
-
-func aboutPage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi\nThis is a credencials generator for AAUE")  // Add links to the rest of the pages
 }
 
+func aboutPage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi\nThis is a credencials generator for AAUE") // Add links to the rest of the pages
+}
 
 func sayhelloName(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()       // parse arguments, you have to call this by yourself
@@ -37,6 +34,7 @@ func sayhelloName(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(r.Form["url_long"])
 	var name string
 	for k, v := range r.Form {
+		fmt.Println(r.Form)
 		fmt.Println("key:", k)
 		fmt.Println("val:", strings.Join(v, ""))
 		name += strings.Join(v, " ") + " "
@@ -45,12 +43,24 @@ func sayhelloName(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello %s\n your input has been received", name) // send data to client side
 }
 
-
-/*func handleUpload(w http.ResponseWriter, r *http.Request) {
-	if r.Method == 'POST'{
-		
+func handleUpload(w http.ResponseWriter, r *http.Request) {
+	in, header, err := r.FormFile("file")
+	errCount := 0
+	if err != nil {
+		log.Fatalf("Error: ", err)
+		errCount += 1
 	}
-}*/
+	defer in.Close()
+	//you probably want to make sure header.Filename is unique and
+	// use filepath.Join to put it somewhere else.
+	out, err := os.OpenFile(header.Filename, os.O_WRONLY|os.O_CREATE, 0644)
+	if err != nil {
+		errCount += 1
+	}
+	defer out.Close()
+	io.Copy(out, in)
+	//do other stuff
+}
 
 func login(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("method:", r.Method) //get request method
@@ -62,27 +72,25 @@ func login(w http.ResponseWriter, r *http.Request) {
 		// logic part of log in
 		fmt.Println("username:", r.Form["username"])
 		fmt.Println("password:", r.Form["password"])
-		/*if len(r.Form["username"]) > 0 && len(r.Form["password"]) > 0 {
-			login(w, r)
-			// or
+		if len(r.Form["username"]) > 0 && len(r.Form["password"]) > 0 {
 			sayhelloName(w, r)
-		}*/
+		}
 	}
 }
 
-
 func main() {
-	/*	portstring := strconv.Itoa('8081')
+	/*
+		portstring := strconv.Itoa('8081')
 		mux := http.NewServeMux()
-		// With a multiplexer DoS will be harder
 		mux.Handle("/",http.HandleFunc(sayhelloName))
 		mux.Handle("/login/",http.HandleFunc(login))         // set router
 		mux.Handle("/cred/",http.HandleFunc(cred))           // Login must always come first
 		mux.Handle("/about/", http.HandleFunc(aboutPage))
 		mux.Handle(http.NotFoundHandle(), http.HandleFunc(aboutPage))
-		*/
+	*/
 	http.HandleFunc("/", sayhelloName)
-	http.HandleFunc("/login", login)         // set router
+	http.HandleFunc("/login/", login) // set router
+	http.HandleFunc("/about/", aboutPage)
 	//http.HandleFunc("/cred", cred)           // Login must always come first
 	err := http.ListenAndServe(":8081", nil) // set listen port
 	fmt.Println("Server up")
